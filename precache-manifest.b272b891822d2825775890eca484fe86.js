c9d37b87d7b3126ab0ec9f79ee3f3958dc26d555
Alan Mauricio <alanmauricioc@ouitlook.com>
2022-05-11 10:25:14 -0500

    Updates

---
+++ precache-manifest.b272b891822d2825775890eca484fe86.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c19befaef14ff4e0329fcb7f07907079",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9eb16e73e2d577f5001",
+    "url": "/static/css/main.f68a1e6f.chunk.css"
+  },
+  {
+    "revision": "36d7818663f5ae3dd2fc",
+    "url": "/static/js/2.3353dd0b.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.3353dd0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9eb16e73e2d577f5001",
+    "url": "/static/js/main.faa4a0dd.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "10550ee86b47ea33b21c4df8c6a053e8",
+    "url": "/static/media/css.10550ee8.png"
+  },
+  {
+    "revision": "fb1e7f9db2540c3194a9179094a925e2",
+    "url": "/static/media/html.fb1e7f9d.png"
+  },
+  {
+    "revision": "652fdb659a681116811612e0b9e25354",
+    "url": "/static/media/java.652fdb65.png"
+  },
+  {
+    "revision": "c647884d2940f119b665a453f9c2c8f3",
+    "url": "/static/media/js.c647884d.png"
+  },
+  {
+    "revision": "bab2c760c60f17191cb3a002e08a3dbf",
+    "url": "/static/media/mysql.bab2c760.png"
+  },
+  {
+    "revision": "772175ff1df43fd3c0bd3b594574cd68",
+    "url": "/static/media/node.772175ff.png"
+  },
+  {
+    "revision": "3faea63afe7015af0fedd5ccc3a2e707",
+    "url": "/static/media/php.3faea63a.png"
+  }
+]);
